af86634d6a047239c3c076aa7dc17e35399de395
Vinicius Lourenço Ferreira <lourencovinicius00@gmail.com>
2022-11-18 06:53:13 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener("click", () => {
+    console.log('cliquei')
+});
+
+function dizOi (nome) {
+    console.log(nome)
+}
+
+dizOi('sas');
